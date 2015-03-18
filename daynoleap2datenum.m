ae915412458d905d8684dbebf1375e6917e28732
Kelly Kearney <kakearney@gmail.com>
2015-03-18 10:02:37 -0700

    Corrected skipped-day bug and added datetime support
    In the original version, I accidentally skipped Mar 1 instead of Feb 29 in leap
    years; this has been corrected.  I also added a third input argument, allowing the
    function to return output as either datenumbers or the new datetime arrays (2014b+ only).

--- daynoleap2datenum.m
+++ daynoleap2datenum.m
@@ -1,10 +1,11 @@
-function dn = daynoleap2datenum(day, pivotyr)
+function dn = daynoleap2datenum(day, pivotyr, format)
 %DAYNOLEAP2DATENUM Convert from days since, no leap to serial date number
 %
 % dn = daynoleap2datenum(day, pivotyr)
+% dt = daynoleap2datenum(day, pivotyr, format)
 %
-% A lot of model output is saved with a time scale of "days since
-% YYYY-01-01 00:00:00", where every year has 365 day.  This function
+% A lot of climate model output is saved with a time scale of "days since
+% YYYY-01-01 00:00:00", where every year has 365 days.  This function
 % converts those dates into a serial date number.  This can be useful if
 % you want to compare model output to real-world data.
 %
@@ -13,8 +14,23 @@ function dn = daynoleap2datenum(day, pivotyr)
 %   day:        number of days since pivot year
 %
 %   pivotyr:    pivot year, i.e. year that day count begins (on Jan 1)
+%
+%   format:     output format, either 'dn' for datenumbers or 'dt' for
+%               datetime objects (R2014b+ only)
+%
+% Output variables:
+%
+%   dn:         array of datenumbers, same size as day
+%
+%   dt:         datetime array, same size as day
 
-% Copyright 2012 Kelly Kearney
+% Copyright 2012-2015 Kelly Kearney
+
+% Parse format
+
+if nargin < 3
+    format = 'dn';
+end
 
 % Determine which years in range are leap years
 
@@ -29,8 +45,23 @@ leapyrs = yrs(isleap(yrs));
 dayofyear = rem(day, 365);
 yr = floor(day/365) + pivotyr;
 
-dn = datenum(pivotyr, 1, 1) + day;
+switch format
+    case 'dn'
+        dn = datenum(pivotyr, 1, 1) + day;
+    case 'dt'
+        if verLessThan('matlab', '8.4.0')
+            warning('Datetime objects not supported pre-R2014b; defaulting to datenumber output');
+            dn = datenum(pivotyr, 1, 1) + day;
+        else
+            dn = datetime(pivotyr, 1, 1) + day;
+        end
+    otherwise
+        error('Unrecognized output format; must be ''dn'' or ''dt''');
+end
+
+% Skip Feb 29 in leap years
+
 for ileap = 1:length(leapyrs)
-    needsbump = yr > leapyrs(ileap) | (yr == leapyrs(ileap) & dayofyear > 59);
+    needsbump = yr > leapyrs(ileap) | (yr == leapyrs(ileap) & dayofyear >= 59);
     dn(needsbump) = dn(needsbump) + 1;
 end
